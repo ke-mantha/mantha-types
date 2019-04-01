15cd29d8f033b01d4d1a41371c1b38f1e14e1b33
Karen <dev.kaskar2008@gmail.com>
2019-04-01 19:47:17 +0300

    Add [index.d.ts]: base types

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+interface Window {
+  [key: string]: any
+}
+
+type ComponentImportPromise = () => Promise<any>;
+type ComponentImportMap = { [name: string]: string | ComponentImportPromise };
+type ComponentImportMapOrArray = ComponentImportMap | Array<string | ComponentImportMap>;
+
+type UseComponentsFunction = {
+  (componentsMap: ComponentImportMapOrArray, relativePath: string): ExtendedUseComponents;
+  (componentsMap: ComponentImportMapOrArray): ExtendedUseComponents;
+  (componentsMap: ComponentImportMapOrArray, relativePath: string): ComponentImportPromiseMap;
+  (componentsMap: ComponentImportMapOrArray): ComponentImportPromiseMap;
+};
+
+type ExtendedUseComponents = ComponentImportPromiseMap & {
+  and: UseComponentsFunction;
+  with: UseComponentsFunction;
+  fluttershy: UseComponentsFunction;
+}
+
+type ComponentImportPromiseMap = {
+  [name: string]: ComponentImportPromise;
+};
+
+declare module '.*' {
+  const render: <T>(arg: T) => T
+  export = render;
+}
